73998f474e3aded0dc3451ee462b30a5692e012e
Christian Jaeger <ch@christianjaeger.ch>
2026-07-17 20:23:18 +0200

    Verify the truth to answer question about group length

--- src/main.rs
+++ src/main.rs
@@ -345,8 +345,9 @@ fn split_hunk<'a>(hunks: &'a str) -> Result<Vec<String>> {
 
         let group_minus_len = group.iter().filter(|l| l.starts_with('-')).count();
         // XXX is it safe to assume that ?
-        // group_minus_len = group.len() - group_minus_len
-        let group_plus_len = group.iter().filter(|l| l.starts_with('+')).count();
+        let group_plus_len = group.len() - group_minus_len;
+        let group_plus_len_verify = group.iter().filter(|l| l.starts_with('+')).count();
+        assert_eq!(group_plus_len, group_plus_len_verify);
 
         let orig_len = new_pre_len + group_minus_len + new_post_len;
         let patched_len = new_pre_len + group_plus_len + new_post_len;
