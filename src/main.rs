e3b314192dabdee0dae0a7b6a00059a408984235
Christian Jaeger <ch@christianjaeger.ch>
2026-07-19 12:02:55 +0200

    Make Hunk and Changes abstractions based on lines, report line numbers (-> buggy)
    - Keep the lines representation through the whole process, i.e. never
      join back into strings to then split or regex-search again; only in
      the end, collect output into `Vec<u8>` buffers via `write_to`
      methods (for the body--the patch heads are still separately
      generated in the old way), then use unbuffered output files and
      `write_vectored` to write those pairs of buffers out (BTW, there's
      an unstable `write_all_vectored` coming that will obviate the need
      for checking the `n_written` count in a year or two.)
    
    - Comment "use slice instead" is now solved instead by using the line
      iterator all the way (no more searching for a header via regex to
      take a string slice).
    
    - Rename types (`ParsedDiff` -> `Diff`) and variables (`diff` ->
      `diff_str`, `parsed_diff` -> `diff`) while at it.
    
    - Instead of trying to generate the same type for hunks and changes to
      allow to share the iteration afterwards (it was String), iterate
      separately over those; this changes behaviour in that the serial
      number inserted into the subject lines and output file names is no
      longer a single hunk/changes counter, but a hierarchical
      "{hunk}-{change}" string now; I think this may even be preferable
      for usability. (BREAKING CHANGE.)
    
    - Make new `Hunk` and `Changes` types, and move formatting
      functionality into methods on them.
    
    - Count line numbers and keep them paired with the line strings
      throughout, and use them where errors refer to particular lines (did
      I miss some places?)
    
    - fn parse_diff: while doing the change to handle line numbers, also
      change to not use mutated variables--to me shadowing appears no more
      complicated, and easier to follow (the IDE allows jumping to where
      the value came from, and having shadowing only instead of a mix with
      `mut` makes tracking things easier).
    
    - fn gather_hunks: use `split_before` from a previous commit
    
    - fn split_hunk_into_changes: use `get_str_and_parse` from a previous
      commit; apparently `Index` on `Capture` breaks life times (ties the
      &str lifetime to the intermediate objects; looks like a bug in the
      `regex` crate); thus use `get(i).unwrap().as_str()`, but hide that
      via the abstraction.
    
      Rename `rest2` to `rest` and shadow the previous `rest`; I find it
      is less error prone for such chained variables (no way to choose the
      wrong one as long as accessing one is still in the correct place in
      the time sequence). (-> In hind sight turns out that I missed that
      the code *did* use the earlier variables, and introduced a bug, then
      made the bug worse with "Remove surplus end condition check" which
      only made sense because of this first error.)

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,22 @@ pub mod utils;
 use std::{
     borrow::Cow,
     fs::read_to_string,
-    io::{BufWriter, Write, stdout},
+    io::{BufWriter, IoSlice, Write, stdout},
     os::unix::ffi::OsStrExt,
     path::{Path, PathBuf},
     sync::Arc,
 };
 
-use anyhow::{Context, Ok, Result, anyhow, bail};
-use cj_path_util::temp_file::temp_file_for;
+use anyhow::{Context, Result, anyhow, bail};
+use cj_path_util::temp_file::unbuffered_temp_file_for;
 use clap_with_warnings::clap_with_warnings;
 use itertools::Itertools;
 use regex::Captures;
 
-use crate::utils::{add_suffix, take_while};
+use crate::{
+    re::GetStr,
+    utils::{add_suffix, split_before, take_while},
+};
 
 #[derive(Debug, clap::Args)]
 struct SplitOptions {
@@ -52,12 +55,12 @@ struct Args {
 /// Returns the list of files created
 fn write_diff(
     head: &[&str],
-    diff: &str,
+    diff_str: &str,
     original_path: &Path,
     split_options: &SplitOptions,
 ) -> Result<Vec<Arc<Path>>> {
     let cap = re!(r"^diff.* (\S+)")
-        .captures(diff)
+        .captures(diff_str)
         .context("missing 'diff' marker with file in the first line of the diff")?;
     let prefix = {
         let file = &cap[1];
@@ -72,44 +75,45 @@ fn write_diff(
     assert_ne!(*path, *original_path);
 
     if split_options.hunks {
-        let parsed_diff = parse_diff(diff)?;
-        let hunks = if split_options.changes {
-            parsed_diff
-                .hunks
-                .into_iter()
-                .map(|hunk| split_hunk_into_changes(&hunk))
-                .collect::<Result<Vec<Vec<String>>>>()?
-                .into_iter()
-                .flatten()
-                .collect()
-        } else {
-            parsed_diff.hunks
-        };
+        let diff = parse_diff(diff_str)?;
+
+        let diff_head = diff.head();
 
         let mut written_paths = Vec::new();
-        for (idx, hunk) in hunks.iter().enumerate() {
-            let diff = [
-                Some(parsed_diff.diff_line),
-                parsed_diff.newfile_line,
-                Some(parsed_diff.minus_line),
-                Some(parsed_diff.plus_line),
-                Some(hunk),
-            ]
-            .into_iter()
-            .flatten()
-            .join("\n");
-
-            written_paths.push(write_patch_file(
-                head_with_subject_prefix(head, format!("{prefix} {idx:03}: "), original_path),
-                &diff,
-                add_suffix(&path, &format!("-{:03}", idx))?.into(),
-            )?);
+        for (hunk_i, hunk) in diff.hunks.iter().enumerate() {
+            if split_options.changes {
+                for (change_i, change) in split_hunk_into_changes(hunk)?.into_iter().enumerate() {
+                    let mut diff_string: Vec<u8> = diff_head.clone().into();
+                    change.write_to(&mut diff_string)?;
+                    written_paths.push(write_patch_file(
+                        head_with_subject_prefix(
+                            head,
+                            format!("{prefix} {hunk_i:03}-{change_i:03}: "),
+                            original_path,
+                        ),
+                        &diff_string,
+                        add_suffix(&path, &format!("-{hunk_i:03}-{change_i:03}"))?.into(),
+                    )?);
+                }
+            } else {
+                let mut diff_string: Vec<u8> = diff_head.clone().into();
+                hunk.write_to(&mut diff_string)?;
+                written_paths.push(write_patch_file(
+                    head_with_subject_prefix(
+                        head,
+                        format!("{prefix} {hunk_i:03}: "),
+                        original_path,
+                    ),
+                    &diff_string,
+                    add_suffix(&path, &format!("-{hunk_i:03}"))?.into(),
+                )?);
+            }
         }
         Ok(written_paths)
     } else {
         Ok(vec![write_patch_file(
             head_with_subject_prefix(head, format!("{}: ", prefix), original_path),
-            diff,
+            diff_str.as_bytes(),
             path,
         )?])
     }
@@ -133,89 +137,179 @@ fn head_with_subject_prefix(head: &[&str], prefix: String, original_path: &Path)
     }
 }
 
-fn write_patch_file(new_head: String, diff: &str, output_path: PathBuf) -> Result<Arc<Path>> {
-    let mut file = temp_file_for(output_path, None)?;
-    (|| {
-        file.write_all(new_head.as_bytes())?;
-        file.write_all(diff.as_bytes())?;
-        // End the diff file with a newline
-        if !diff.ends_with('\n') {
-            eprintln!("doesnt end with newline");
-            file.write_all(b"\n")?;
-        }
-        Ok(())
-    })()
-    .with_context(|| anyhow!("writing to {:?}", file.temp_path()))?;
+fn write_patch_file(new_head: String, diff: &[u8], output_path: PathBuf) -> Result<Arc<Path>> {
+    let new_head = new_head.as_bytes();
+    let expected_n_written = new_head.len() + diff.len();
+    let mut file = unbuffered_temp_file_for(&*output_path, None)?;
+    let n_written = file
+        .write_vectored(&[IoSlice::new(new_head), IoSlice::new(diff)])
+        .with_context(|| anyhow!("writing to {:?}", file.temp_path()))?;
+    if n_written != expected_n_written {
+        bail!("could only write {n_written} out of {expected_n_written} bytes to {output_path:?}");
+    }
     Ok(file.persist()?)
 }
 
-struct ParsedDiff<'a> {
+/// A group of lines starting with a "@@" line and not containing
+/// other such lines; contains any number of changes
+struct Hunk<'a> {
+    /// The Vec is never empty, at least the "@@ " line is ensured by
+    /// construction via `split_before` which does not create a group
+    /// out of no lines.
+    lines: Vec<(usize, &'a str)>,
+}
+
+impl<'a> Hunk<'a> {
+    fn write_to(&self, mut out: impl Write) -> Result<(), std::io::Error> {
+        for (_, line) in &self.lines {
+            writeln!(&mut out, "{line}")?;
+        }
+        Ok(())
+    }
+}
+
+/// A single group of "-" and "+" lines and context around them; a
+/// number of changes make up a hunk
+struct Change<'a, 'h> {
+    orig_start: usize,
+    orig_len: usize,
+    patched_start: usize,
+    patched_len: usize,
+    head_post: &'a str,
+    pre: &'h [(usize, &'a str)],
+    group: &'h [(usize, &'a str)],
+    post: &'h [(usize, &'a str)],
+}
+
+impl<'a, 'h> Change<'a, 'h> {
+    fn write_to(&self, mut out: impl Write) -> Result<(), std::io::Error> {
+        let Self {
+            orig_start,
+            orig_len,
+            patched_start,
+            patched_len,
+            head_post,
+            pre,
+            group,
+            post,
+        } = self;
+        // XXX: Does the header need to be adapted to the following
+        // patterns? As discovered for `split_hunk`
+        // @@ -0,0 +1,2 @@
+        // @@ -42 42 @@
+        // @@ -42 +1,2 @@
+        // @@ -0,0 +1 @@
+        writeln!(
+            &mut out,
+            "@@ -{},{} +{},{} {}",
+            orig_start, orig_len, patched_start, patched_len, head_post
+        )?;
+        for (_, line) in *pre {
+            writeln!(&mut out, "{line}")?;
+        }
+        for (_, line) in *group {
+            writeln!(&mut out, "{line}")?;
+        }
+        for (_, line) in *post {
+            writeln!(&mut out, "{line}")?;
+        }
+        Ok(())
+    }
+}
+
+/// A bare diff for a single file. (A Patch file represents any number
+/// of Diff instances.)
+struct Diff<'a> {
     diff_line: &'a str,
     newfile_line: Option<&'a str>,
     #[allow(unused)]
     index_line: Option<&'a str>,
     minus_line: &'a str,
     plus_line: &'a str,
-    hunks: Vec<String>,
-    // hunks: Vec<&'a str>,
+    hunks: Vec<Hunk<'a>>,
 }
 
-fn parse_diff(diff: &str) -> Result<ParsedDiff<'_>> {
-    // XXX: use slice instead
-    let mut lines = diff.lines();
+impl<'a> Diff<'a> {
+    /// Not the head of the patch (i.e. mail headers / commit
+    /// message), but of this diff. Ends with a newline.
+    fn head(&self) -> String {
+        let Self {
+            diff_line,
+            newfile_line,
+            index_line: _,
+            minus_line,
+            plus_line,
+            hunks: _,
+        } = self;
+        [
+            Some(*diff_line),
+            *newfile_line,
+            Some(*minus_line),
+            Some(*plus_line),
+        ]
+        .into_iter()
+        .flatten()
+        .join("\n")
+            + "\n"
+    }
+}
 
-    let diff_line = lines
+fn parse_diff(diff: &str) -> Result<Diff<'_>> {
+    let mut lines = diff.lines().enumerate();
+
+    let (_, diff_line) = lines
         .next()
-        .filter(|l| l.starts_with("diff "))
-        .with_context(|| format!("invalid patch file format [missing diff line]: {diff}"))?;
+        .filter(|(_, l)| l.starts_with("diff "))
+        .with_context(|| format!("invalid patch file format: missing 'diff ' line"))?;
 
-    let mut line = lines
+    let (line0, line) = lines
         .next()
-        .with_context(|| format!("invalid patch file format [diff ended unexpectedly]: {diff}"))?;
+        .with_context(|| format!("invalid patch file format: unexpected EOF after 'diff ' line"))?;
 
-    let newfile_line = if line.starts_with("new file mode ") {
-        let l = line;
-        line = lines.next().with_context(|| {
-            format!("invalid patch file format [missing `index` or `---` line]: {diff}")
-        })?;
-        Some(l)
+    let (newfile_line, (line0, line)) = if line.starts_with("new file mode ") {
+        (
+            Some(line),
+            lines.next().with_context(|| {
+                format!("invalid patch file format: [missing `index` or `---` line]")
+            })?,
+        )
     } else {
-        None
+        (None, (line0, line))
     };
 
-    let index_line = if line.starts_with("index ") {
-        let l = line;
-        line = lines
-            .next()
-            .with_context(|| format!("invalid patch file format [missing `---` line]: {diff}"))?;
-        Some(l)
+    let (index_line, (line0, line)) = if line.starts_with("index ") {
+        (
+            Some(line),
+            lines.next().with_context(|| {
+                format!("invalid patch file format [missing `---` line]: {diff}")
+            })?,
+        )
     } else {
-        None
+        (None, (line0, line))
     };
 
     if !line.starts_with("--- ") {
-        bail!("invalid patch file format [expected `---` line]: {diff}");
+        bail!(
+            "invalid patch file format: expected `---` on line {}",
+            line0 + 1
+        );
     }
     let minus_line = line;
 
-    line = lines
+    let (line0, line) = lines
         .next()
         .with_context(|| format!("invalid patch file format [missing `+++` line]: {diff}"))?;
     if !line.starts_with("+++ ") {
-        bail!("invalid patch file format [expected `+++` line]: {diff}");
+        bail!(
+            "invalid patch file format: expected `+++` on line {}",
+            line0 + 1
+        );
     }
     let plus_line = line;
 
-    // rest: everything after `+++` line
-    let plus_start = diff
-        .find(plus_line)
-        .context("Should have found the `+++` line")?;
-    let rest_start = plus_start + plus_line.len() + 1;
-    let rest = &diff[rest_start..];
+    let hunks = gather_hunks(lines);
 
-    let hunks = gather_hunks(rest).into_iter().map(str::to_owned).collect();
-
-    Ok(ParsedDiff {
+    Ok(Diff {
         diff_line,
         newfile_line,
         index_line,
@@ -225,24 +319,19 @@ fn parse_diff(diff: &str) -> Result<ParsedDiff<'_>> {
     })
 }
 
-fn gather_hunks(s: &str) -> Vec<&str> {
-    let mut hunks = Vec::new();
-    let mut start = 0;
-
-    for (idx, _) in s.match_indices("\n@@ ") {
-        let split_at = idx + 1; // newline stays with previous hunk
-        hunks.push(&s[start..split_at]);
-        start = split_at
-    }
-
-    hunks.push(&s[start..]);
-    hunks
+fn gather_hunks<'s>(lines: impl Iterator<Item = (usize, &'s str)>) -> Vec<Hunk<'s>> {
+    split_before(
+        lines,
+        |(_, line)| line.starts_with("@@ "),
+        |group| Hunk { lines: group },
+    )
 }
 
-fn split_hunk_into_changes<'a>(hunk: &'a str) -> Result<Vec<String>> {
-    let lines: Vec<&'a str> = hunk.lines().collect();
-
-    let head = lines.first().context("hunk is empty")?;
+fn split_hunk_into_changes<'a, 'h>(hunk: &'h Hunk<'a>) -> Result<Vec<Change<'a, 'h>>> {
+    let (head_line_line0, head_line) = hunk
+        .lines
+        .first()
+        .expect("hunks are expected to never be empty by construction");
 
     // XXX: are all these valid patterns? What happens if captures
     // fail?
@@ -251,20 +340,20 @@ fn split_hunk_into_changes<'a>(hunk: &'a str) -> Result<Vec<String>> {
     // @@ -42 +1,2 @@
     // @@ -0,0 +1 @@
     let caps = re!(r"^@@ -(\d+)(?:,(\d+))? \+(\d+)(?:,(\d+))? (.*)")
-        .captures(head)
-        .with_context(|| format!("invalid hunk head: {head}"))?;
+        .captures(head_line)
+        .with_context(|| format!("invalid hunk head: {head_line}"))?;
 
-    let mut orig_start: usize = caps[1].parse()?;
-    let mut patched_start: usize = caps[3].parse()?;
-    let head_post = &caps[5];
+    let mut orig_start: usize = caps.get_str_and_parse(1, *head_line_line0)?;
+    let mut patched_start: usize = caps.get_str_and_parse(3, *head_line_line0)?;
+    let head_post = caps.get_str(5);
 
-    let mut remaining: &[&str] = &lines[1..];
+    let mut remaining: &[(usize, &str)] = &hunk.lines[1..];
     let mut result = Vec::new();
 
     while !remaining.is_empty() {
-        let (pre, rest) = take_while(remaining, |l| l.starts_with(' '));
-        let (group, rest2) = take_while(rest, |l| l.starts_with(['-', '+']));
-        let (post, rest3) = take_while(rest2, |l| l.starts_with(' '));
+        let (pre, rest) = take_while(remaining, |(_, l)| l.starts_with(' '));
+        let (group, rest) = take_while(rest, |(_, l)| l.starts_with(['-', '+']));
+        let (post, rest) = take_while(rest, |(_, l)| l.starts_with(' '));
 
         let pre_len = pre.len();
 
@@ -278,7 +367,7 @@ fn split_hunk_into_changes<'a>(hunk: &'a str) -> Result<Vec<String>> {
         let new_post = if post.len() > 3 { &post[..3] } else { post };
         let new_post_len = new_post.len();
 
-        let group_minus_len = group.iter().filter(|l| l.starts_with('-')).count();
+        let group_minus_len = group.iter().filter(|(_, l)| l.starts_with('-')).count();
         // The group consists purely of lines starting with '-' and
         // '+' by its construction, hence:
         let group_plus_len = group.len() - group_minus_len;
@@ -286,32 +375,25 @@ fn split_hunk_into_changes<'a>(hunk: &'a str) -> Result<Vec<String>> {
         let orig_len = new_pre_len + group_minus_len + new_post_len;
         let patched_len = new_pre_len + group_plus_len + new_post_len;
 
-        // XXX: Does the header need to be adapted to the following
-        // patterns? As discovered for `split_hunk`
-        // @@ -0,0 +1,2 @@
-        // @@ -42 42 @@
-        // @@ -42 +1,2 @@
-        // @@ -0,0 +1 @@
-        let header = format!(
-            "@@ -{},{} +{},{} {}",
-            orig_start, orig_len, patched_start, patched_len, head_post
-        );
+        result.push(Change {
+            orig_start,
+            orig_len,
+            patched_start,
+            patched_len,
+            head_post,
+            pre: new_pre,
+            group,
+            post: new_post,
+        });
 
-        let mut out = vec![header];
-        out.extend(new_pre.iter().map(|s| s.to_string()));
-        out.extend(group.iter().map(|s| s.to_string()));
-        out.extend(new_post.iter().map(|s| s.to_string()));
-
-        result.push(out.join("\n"));
-
-        if rest3.is_empty() {
+        if rest.is_empty() {
             break;
         }
 
         orig_start += pre_len + group_minus_len;
         patched_start += pre_len + group_plus_len;
 
-        remaining = rest2;
+        remaining = rest;
     }
 
     Ok(result)
@@ -362,7 +444,7 @@ fn main() -> Result<()> {
             .with_context(|| anyhow!("splitting the patch file {patch_file:?}"))?;
 
         if !args.quiet {
-            (|| {
+            (|| -> Result<_> {
                 let mut out = BufWriter::new(stdout().lock());
                 for path in written {
                     out.write_all(path.as_os_str().as_bytes())?;
