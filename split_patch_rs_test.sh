e932a826f216431cbbffd72434b0447262ea8c79
Yusuf Raji <raji.yusuf234@gmail.com>
2026-07-21 11:52:08 +0200

    Add test script
    Compares the result of split-patch's perl version with the Rust version.

---
+++ split_patch_rs_test.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env sh
+set -eux
+
+# path to perl version of split-patch
+SPLIT_PATCH_PL="/opt/chj/bin/split-patch";
+# path to rust version of split-patch
+SPLIT_PATCH_RS=...;
+
+DIR="/tmp"
+# commit sha for `git format-patch`
+SHA=...
+
+PATCH_DIR=$DIR/PATCHES
+mkdir -p $PATCH_DIR
+rm -rf $PATCH_DIR/*
+
+git format-patch -o $PATCH_DIR $SHA 
+
+for opt in "hunks" "changes" "quiet" ; do
+    echo "Working on option: $opt"
+    pl_dir="$DIR/pl/$opt"
+    mkdir -p $pl_dir
+    rm -rf $pl_dir/*
+    rs_dir="$DIR/rs/$opt"
+    mkdir -p $rs_dir
+    rm -rf $rs_dir/*
+
+    cp $PATCH_DIR/* $pl_dir
+    "$SPLIT_PATCH_PL" "--$opt" "$pl_dir"/*
+
+    cp $PATCH_DIR/* $rs_dir
+    "$SPLIT_PATCH_RS" "--$opt" "$rs_dir"/*
+
+    diff -ru $pl_dir $rs_dir
+done
