1789170da7d7a0fc119d8402861c5daab79744c3
Christian Jaeger <ch@christianjaeger.ch>
2026-07-22 11:04:39 +0200

    split_patch_rs_test.sh: * does not include dot files

--- split_patch_rs_test.sh
+++ split_patch_rs_test.sh
@@ -11,19 +11,19 @@ dir=$(mktemp -d)
 revision_range=...
 
 patch_dir=$dir/PATCHES
+rm -rf "$patch_dir"
 mkdir -p "$patch_dir"
-rm -rf "${patch_dir:?}"/*
 
 git format-patch -o "$patch_dir" "$revision_range"
 
 for opt in "hunks" "changes" "quiet" ; do
     echo "Working on option: $opt"
     pl_dir="$dir/pl/$opt"
+    rm -rf "$pl_dir"
     mkdir -p "$pl_dir"
-    rm -rf "${pl_dir:?}"/*
     rs_dir="$dir/rs/$opt"
+    rm -rf "$rs_dir"
     mkdir -p "$rs_dir"
-    rm -rf "${rs_dir:?}"/*
 
     cp "$patch_dir"/* "$pl_dir"
     "$split_patch_pl" "--$opt" "$pl_dir"/*
