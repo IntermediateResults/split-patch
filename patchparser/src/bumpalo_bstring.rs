7a94f78bce68fd97aae4c09591d985daeffaae4e
Christian Jaeger <ch@christianjaeger.ch>
2026-08-02 23:44:21 +0200

    patchparser: add bumpalo_bstring.rs

---
+++ patchparser/src/bumpalo_bstring.rs
@@ -0,0 +1,31 @@
+use std::ops::{Deref, DerefMut};
+
+use bstr::BStr;
+use bumpalo::{collections::Vec, Bump};
+
+/// like `bstr::BString` but allocating from `bumpalo::Bump`
+pub struct BString<'bump>(Vec<'bump, u8>);
+
+impl<'bump> BString<'bump> {
+    pub fn new_in(bump: &'bump Bump) -> Self {
+        Self(Vec::new_in(bump))
+    }
+
+    pub fn into_bump_slice(self) -> &'bump BStr {
+        self.0.into_bump_slice().as_ref()
+    }
+}
+
+impl<'bump> Deref for BString<'bump> {
+    type Target = Vec<'bump, u8>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<'bump> DerefMut for BString<'bump> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
